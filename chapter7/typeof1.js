42b4ff6b03961212a1dc7bbee2b1180ffe78dbe9
dev.bojack <98508704+devbojack@users.noreply.github.com>
2023-12-17 04:59:42 +0300

    testing types

--- chapter7/typeof1.js
+++ chapter7/typeof1.js
@@ -1,23 +1,19 @@
 
-var test1 = "abcdef"; 
-var test2 = 123;
-var test3 = true; 
-var test4 = {};
-var test5 = [];
-var test6;
-var test7 = {"abcdef": 123};
-var test8 = ["abcdef", 123]; 
-function test9(){
-	return "abcdef"
+var test1 = "abcdef";            //string
+var test2 = 123;                //number
+var test3 = true;               //boolean
+var test4 = {};                 //object
+var test5 = [];                 //object
+var test6;                      //undefined
+var test7 = { "abcdef": 123 };  //object
+var test8 = ["abcdef", 123];    //object
+function test9() {              //function
+    return "abcdef"
 };
-var test10 = null;
-console.log(typeof test1); 
-console.log(typeof test2);
-console.log(typeof test3); 
-console.log(typeof test4);
-console.log(typeof test5); 
-console.log(typeof test6);
-console.log(typeof test7); 
-console.log(typeof test8);
-console.log(typeof test9);
-console.log(typeof test10);
+var test10 = null;              //object
+
+var allTest = [test1, test2, test3, test4, test5, test6, test7, test8, test9, test10];
+
+for (var i =0; i < allTest.length; i++){
+    console.log(typeof allTest[i]);
+}
