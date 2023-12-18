95f3c469e197e45ada025f8df2037e2e0c58a4dd
dev.bojack <98508704+devbojack@users.noreply.github.com>
2023-12-18 11:08:32 +0300

    eightball random, non-random

--- chapter5/eightball.js
+++ chapter5/eightball.js
@@ -1,34 +1,34 @@
 var eightBall = {
-    balls: ["One","Two","Three","Four","Five","Six","Seven", "Eight"],
-    shake: function(){
-        return this.balls[Math.floor(Math.random() * this.balls.length)];
-    },
-    display: function(){
-        console.log(this.shake());
-    }
+	balls: ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight"],
+	shake: function () {
+		return this.balls[Math.floor(Math.random() * this.balls.length)];
+	},
+	display: function () {
+		console.log(this.shake());
+	}
 }
 eightBall.display();
 
 
-var eightBall = { 
-	index: 0,  
+var eightBall = {
+	index: 0,
 	advice: ["yes", "no", "maybe", "not a chance"],
-	shake: function() { 
+	shake: function () {
 		this.index = this.index + 1;
-		if (this.index  >= this.advice.length) {
+		if (this.index >= this.advice.length) {
 			this.index = 0;
 		}
 	},
-	look: function() {
+	look: function () {
 		return this.advice[this.index];
 	}
-};   
-	
-eightBall.shake();	
+};
+
+eightBall.shake();
 console.log(eightBall.look());
 
-eightBall.shake();	
+eightBall.shake();
 console.log(eightBall.look());
 
-eightBall.shake();	
+eightBall.shake();
 console.log(eightBall.look());
